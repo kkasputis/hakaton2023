2cbc5636c9d1b50ca6e3304007d880214e62e287
Kipras <kipras.kasputis@ibm.com>
2023-11-25 22:34:10 +0200

    3rd  push

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-let express = require('espress');
-
-let app = express;
-
-app.use(express.static(__dirname+'/dist/hakaton-2023'));
-
-app.get('/*', (req, resp) => {
-    resp.sendFile(__dirname+'/dist/hakaton-2023/index.html')
-})
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/<app-name>'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/<app-name>/index.html'));});
+app.listen(process.env.PORT || 8080);
